849688c8a79b0109d444148b26c8a9bf4fec5cd4
AdrianJoldes <ady_joldes@yahoo.com>
2017-12-09 17:54:33 +0200

    Data generator second version

---
+++ GenerateData/GenerateData/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace GenerateData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Generate_Data = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Generate_Data
+            // 
+            this.button_Generate_Data.Location = new System.Drawing.Point(139, 42);
+            this.button_Generate_Data.Name = "button_Generate_Data";
+            this.button_Generate_Data.Size = new System.Drawing.Size(211, 69);
+            this.button_Generate_Data.TabIndex = 0;
+            this.button_Generate_Data.Text = "Generate";
+            this.button_Generate_Data.UseVisualStyleBackColor = true;
+            this.button_Generate_Data.Click += new System.EventHandler(this.button_Generate_Data_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 142);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 53);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 248);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_Generate_Data);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Generate_Data;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ GenerateData/GenerateData/Form1.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace GenerateData
+{
+    public partial class Form1 : Form
+    {
+        string accountName = "mystorage008";
+        string accountKey = "VnoP7qCYV4r5N22JhrlVUuPURS+R3+3JioE8XR3e5l9O/bRVN7bPMkMGGU7lpHCN1cIr8DmMuDxyN+TCMTG79g==";
+        private CloudQueue queue;
+        LocParcare[] locuriParcare = new LocParcare[96];
+        Random rnd = new Random();
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+        public Form1()
+        {
+            InitializeComponent();
+            // Retrieve storage account from connection string.
+            //CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+            //  CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            //DefaultEndpointsProtocol=https;AccountName=mystorage008;AccountKey=VnoP7qCYV4r5N22JhrlVUuPURS+R3+3JioE8XR3e5l9O/bRVN7bPMkMGGU7lpHCN1cIr8DmMuDxyN+TCMTG79g==;EndpointSuffix=core.windows.net
+            CloudStorageAccount storageAccount = new CloudStorageAccount(new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(accountName, accountKey), true);
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a container.
+            queue = queueClient.GetQueueReference("json-messages");
+
+            // Create the queue if it doesn't already exist
+            queue.CreateIfNotExists();
+
+
+
+        }
+
+        private void button_Generate_Data_Click(object sender, EventArgs e)
+        {
+            string jsonString;
+            int r1 = rnd.Next(0, 2);
+            if (r1 == 0)
+            {
+                jsonString = generateGoodData();
+                textBox1.Text = "Date bune";
+
+            }
+            else
+            {
+                jsonString = generateBadData();
+                textBox1.Text = "Date rele";
+            }
+
+
+            // Create a message and add it to the queue.
+            CloudQueueMessage message = new CloudQueueMessage(jsonString);
+            queue.AddMessage(message);
+
+        }
+
+        private string generateBadData()
+        {
+            for (int i = 0; i < 96; i++)
+            {
+                int nr = rnd.Next(0, 2);
+                locuriParcare[i] = new LocParcare();
+                locuriParcare[i].Stare = "state";
+                locuriParcare[i].Id ="200";
+                string loc = "Locul " + locuriParcare[i].Id + " este " + locuriParcare[i].Stare;
+                // Console.WriteLine(locuriParcare[i].Stare);
+            }
+            return JsonConvert.SerializeObject(locuriParcare);
+
+        }
+
+        private string generateGoodData()
+        {
+            for (int i = 0; i < 96; i++)
+            {
+                int nr = rnd.Next(0, 2);
+                locuriParcare[i] = new LocParcare();
+                locuriParcare[i].Stare = ((Stare)nr).ToString();
+                locuriParcare[i].Id = i.ToString();
+                string loc = "Locul " + locuriParcare[i].Id + " este " + locuriParcare[i].Stare;
+                // Console.WriteLine(locuriParcare[i].Stare);
+            }
+            return JsonConvert.SerializeObject(locuriParcare);
+
+        }
+    }
+}
